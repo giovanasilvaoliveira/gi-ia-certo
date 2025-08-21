fa3297b84cb72591b01c1e0a74aa676b2eb43515
Giovana Oliveira <143838386+giovanasilvaoliveira@users.noreply.github.com>
2025-08-21 14:21:20 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,139 @@
+export const perguntas = [
+    {
+        enunciado: "Como você vê o impacto a IA na educação?",
+        alternativas: [
+            {
+                texto: "A IA pode personalisar o aprendizado, oferecendo experiência educacionais mais eficientes e adaptáveis.",
+                afirmacao: [
+                    "Você acredita que IA pode revolucionar a educação, oferecendo materiais e exercícios personalisadospara cada aluno..",
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "A IA pode prejudicar a educação ao substituir professores e desumanizar o processo de aprendizagem.",
+                afirmacao: [
+                    "Você teme que a IA retire o papel fundamental dos profssores e a interação humana no processo educativo",
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Agora imagine que essa pode ser usada em escolas, empregos e até no governo tomando decisões com base em dados. Você confiaria que ela tomasse decisões importantes em seu lugar?",
+        alternativas: [
+            {
+                texto: "Sim, porque ela é lǵica e rápida.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: " Não, porque ela falta humanidade e ética.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você acha que a IA pode influenciar o mercado de trabalho?",
+        alternativas: [
+            {
+                texto: "A IA pode criar novas oportunidades de emprego, especialmente em áreas de tecnologia.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "A IA pode eliminar muitos empregos, deixando as pessoas sem trabalho.",
+                afirmacao: [
+                    "Você tem receio de que a automação excessiva acabe gerando desemprego em larga escala",
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "A IA pode melhorar a saúde, ajudando a diagnosticar doenças mais rapidamente. Qual a sua opinião sobre isso?",
+        alternativas: [
+            {
+                texto: "Sim, pois a IA pode analisar grandes volumes de dados e fazer diagnósticos mais rápidos.",
+                afirmacao: [
+                    "Você acredita que a IA pode salvar vidas ao ser aplicada corretamente no diagnóstico médico.",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Não, a IA pode cometer erros graves sem a supervisão humana.",
+                afirmacao: [
+                    "Você considera que a supervisão humana ainda é essencial, especialmente quando se trata de saúde e vida humana.",
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Como você vê o impacto do ChatGPT na forma como as pessoas fazem pesquisas e escrevem textos?",
+        alternativas: [
+            {
+                texto: "O ChatGPT pode ajudar as pessoas a obter informações rapidamente, facilitando a pesquisa e a escrita de textos.",
+                afirmacao: [
+                    "Você acredita que o ChatGPT é uma ferramenta que pode agilizar o processo de pesquisa e melhorar a produtividade, especialmente para quem tem dificuldades em começar um texto ou encontrar informações.",
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "O ChatGPT pode fazer com que as pessoas se tornem preguiçosas e dependentes da tecnologia para produzir conteúdo.",
+                afirmacao: [
+                    "Você tem receio de que o uso excessivo do ChatGPT para criar textos possa prejudicar a criatividade e a habilidade das pessoas de escrever de forma independente.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Novas tecnologias trazem novos desafios, por isso após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+
+
+
+
+
+            },
+        ]
+    }
+];
